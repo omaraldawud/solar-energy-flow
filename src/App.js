7b2acd4020c933f07fc80bc4fcd6a89a8a583c12
Omar Aldawud <omaldawud@noctrl.edu>
2025-06-11 09:29:42 -0500

    Initial deploy setup with GitHub Actions

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import './App.css';
+export default function App() {
+    return (_jsx("div", { className: "app-container", children: _jsxs("div", { className: "container", children: [_jsx(Header, {}), _jsx(Hero, {}), _jsx(Footer, {})] }) }));
+}
