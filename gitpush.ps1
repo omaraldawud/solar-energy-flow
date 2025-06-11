e360be105645f4f778416015e79bb8cad9a6a0ce
Omar Aldawud <omaldawud@noctrl.edu>
2025-06-11 11:02:51 -0500

    Initial PNW Irrigation Site

---
+++ gitpush.ps1
@@ -0,0 +1,5 @@
+# Save this as, for example, gitpush.ps1
+
+git add .
+git commit -m "Initial PNW Irrigation Site"
+git push -u origin main
--- src/App.js
+++ src/App.js
@@ -4,18 +4,5 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import './App.css';
 export default function App() {
-    return (_jsx("div", { 
-        className: "app-container", 
-        children: _jsxs("div", { 
-            className: "container", 
-            children: [
-                        _jsx(Header, {}), 
-                        _jsx(Hero, {}), 
-                        _jsx(Footer, {})
-                    ] 
-                }
-            ) 
-        }
-    )
-);
+    return (_jsx("div", { className: "app-container", children: _jsxs("div", { className: "container", children: [_jsx(Header, {}), _jsx(Hero, {}), _jsx(Footer, {})] }) }));
 }
