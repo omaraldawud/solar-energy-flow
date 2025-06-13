5aa7850a588252b6a94eb6ca965a1084b4390cd0
Omar Aldawud <omaldawud@noctrl.edu>
2025-06-13 17:17:27 -0500

    Initial PNW Irrigation Site

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Important for relative paths when deploying via FTP
 })
