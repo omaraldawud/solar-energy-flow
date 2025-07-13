5022ed3fd31135961d77ca1ee6dafd2bdbda47b9
Omar Aldawud <omaldawud@noctrl.edu>
2025-07-13 12:23:53 -0500

    Restructured Files v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import HomePage from './pages/HomePage';
 import FreeDesignPage from './pages/FreeDesignPage';
 import { AboutUsPage } from './pages/AboutUsPage';
 import SolarEenergyServices from './pages/SolarEenergyServices';
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/solar-energy-services" element={<SolarEenergyServices />} />
         <Route path="/free-solar-system-design" element={<FreeDesignPage />} />
         <Route path="/about-us" element={<AboutUsPage />} />
